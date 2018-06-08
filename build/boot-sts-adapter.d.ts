770cc603e024a8e2c0300cc8b6914bb6457a9bba
BoykoAlex <aboyko@pivotal.io>
2018-06-08 12:11:19 -0400

    Remove build folder

--- build/boot-sts-adapter.d.ts
+++
@@ -1,7 +0,0 @@
-import { StsAdapter, HighlightParams } from '@pivotal-tools/atom-languageclient-commons';
-export declare class BootStsAdapter extends StsAdapter {
-    constructor();
-    onHighlight(params: HighlightParams): void;
-    private markHintsForEditor;
-    private createHintMarker;
-}
--- build/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const spring_boot_language_client_1 = require("./spring-boot-language-client");
-module.exports = new spring_boot_language_client_1.SpringBootLanguageClient();
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL2xpYi9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsK0VBQXVFO0FBRXZFLE1BQU0sQ0FBQyxPQUFPLEdBQUcsSUFBSSxzREFBd0IsRUFBRSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHtTcHJpbmdCb290TGFuZ3VhZ2VDbGllbnR9IGZyb20gJy4vc3ByaW5nLWJvb3QtbGFuZ3VhZ2UtY2xpZW50JztcblxubW9kdWxlLmV4cG9ydHMgPSBuZXcgU3ByaW5nQm9vdExhbmd1YWdlQ2xpZW50KCk7Il19
